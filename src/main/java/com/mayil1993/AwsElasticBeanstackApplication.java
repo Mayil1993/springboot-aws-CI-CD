ec984d6ff6c6398c4da106a29287b262bff9b6c9
Mayil Vahanan <mavah93@gmail.com>
2020-12-16 01:05:17 +0800

    third commit

---
+++ src/main/java/com/mayil1993/AwsElasticBeanstackApplication.java
@@ -0,0 +1,13 @@
+package com.mayil1993;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticBeanstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticBeanstackApplication.class, args);
+	}
+
+}
