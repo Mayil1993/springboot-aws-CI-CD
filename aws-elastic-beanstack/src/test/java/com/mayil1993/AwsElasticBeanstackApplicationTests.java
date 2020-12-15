03a790484923ed28e52341a01e547eadbb3fa443
Mayil Vahanan <mavah93@gmail.com>
2020-12-16 00:04:35 +0800

    first commit

---
+++ aws-elastic-beanstack/src/test/java/com/mayil1993/AwsElasticBeanstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayil1993;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsElasticBeanstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
